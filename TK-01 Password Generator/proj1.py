c74decea3bef86ff19aed7254c5190719ee96b3b
Irfana-Begam <98166475+Irfana-Begam@users.noreply.github.com>
2024-12-28 13:16:49 +0530

    Update proj1.py

--- TK-01 Password Generator/proj1.py
+++ TK-01 Password Generator/proj1.py
@@ -6,7 +6,7 @@ def generate_password(pas_ln):
     return password
 def generate_password_conditions(pas_ln):
     all_characters = string.ascii_letters + string.digits + string.punctuation
-    pd=[random.choice(string.ascii_letters),random.choice(string.digits),random.choice(string.punctuation)]
+    pd=[random.choice(string.ascii_uppercase),random.choice(string.ascii_lowercase),random.choice(string.digits),random.choice(string.punctuation)]
     password="".join(random.choices(all_characters,k=pas_ln))    
     return password
 pwd=int(input("Enter the length of password here:"))
