e20c1e265de4dc559de87cd77723ef89f0c3b1b3
Irfana-Begam <irfanabegam786786@gmail.com>
2024-12-31 18:03:34 +0530

    task2 updated

--- TK-02 Simple Port Scanner/client.py
+++ TK-02 Simple Port Scanner/client.py
@@ -2,11 +2,9 @@ import socket
 client_socket_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 8080)
 client_socket_object.connect(server_address)
-try:
-    message = "This is a client-side message"
-    client_socket_object.send(message.encode())
-    print(f"Sent to server: {message}")
-    response = client_socket_object.recv(1024).decode()
-    print(f"Received from server: {response}")
-finally:
-    client_socket_object.close()
+message = "This is a client-side message"
+client_socket_object.send(message.encode())
+print(f"Sent to server: {message}")
+response = client_socket_object.recv(1024).decode()
+print(f"Received from server: {response}")
+client_socket_object.close()
